c8294dc031275e788c63683a6bdf28590d918c25
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-02-26 00:29:30 +0100

    bem selector pattern stylelint rule

--- src/configs/.stylelintrc.js
+++ src/configs/.stylelintrc.js
@@ -374,6 +374,13 @@ module.exports = {
         severity: 'warning',
       },
     ],
+    'selector-class-pattern': [
+      '^\.[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$',
+      {
+        message: 'Expected BEM class',
+        severity: 'warning',
+      }
+    ],
     'selector-pseudo-class-no-unknown': true,
     'selector-pseudo-element-no-unknown': true,
     'selector-type-no-unknown': true,
