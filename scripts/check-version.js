1d8667afdfcdaa4afc79f3902ada207decd273e6
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-03-01 21:10:17 +0100

    optimize images, update eslint rules

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,66 +1,74 @@
 const sgf = require('staged-git-files');
 const gtr = require('git-tags-remote');
-const packageJsonData = require('../package.json'); 
+const packageJsonData = require('../package.json');
 
-function checkVersion () {
+function checkVersion() {
   console.info('Checking version…');
 
-  sgf((err, results) => {    
+  sgf((err, results) => {
     // We want to check things:
     // - if at least one file in `src` was modified (deleted), we expect the minor version bumped
     // - if at least one file in `src` was added, but none modified, we expect the patch version bumped
     // - if no file in `src` was changed in any way, we expect version to be untouched
 
-    console.log(results)
+    const hasAddedSrcFiles =
+      results.filter(
+        (result) =>
+          result.filename.startsWith('src/') && result.status === 'Added'
+      ).length >= 1;
 
-    const hasAddedSrcFiles = results.filter((result) => {
-      return result.filename.startsWith('src/') && result.status === 'Added';
-    }).length >= 1;
-
-    const hasModifiedSrcFiles = results.filter((result) => {
-      return (
-        result.filename.startsWith('src/') &&
-        (
-          result.status === 'Modified' ||
-          result.status === 'Deleted' ||
-          result.status === 'Renamed'
-        )
-      );
-    }).length >= 1;
+    const hasModifiedSrcFiles =
+      results.filter(
+        (result) =>
+          result.filename.startsWith('src/') &&
+          (result.status === 'Modified' ||
+            result.status === 'Deleted' ||
+            result.status === 'Renamed')
+      ).length >= 1;
 
     const proposedVersion = packageJsonData.version;
 
-    const isPackageJsonModified = results.find((result) => {
-      return result.filename === 'package.json';
-    });
+    const isPackageJsonModified = results.find(
+      (result) => result.filename === 'package.json'
+    );
 
     if ((hasAddedSrcFiles || hasModifiedSrcFiles) && !isPackageJsonModified) {
-      console.error('You need to commit your package.json file with updated version!');
+      console.error(
+        'You need to commit your package.json file with updated version!'
+      );
       process.exit(1);
     }
 
-    gtr.latest('git@github.com:kobotoolbox/kobo-common.git').then((response) => {
-      const latestVersion = response[0];
-      const latestVersionArray = latestVersion.split('.');
+    gtr
+      .latest('git@github.com:kobotoolbox/kobo-common.git')
+      .then((response) => {
+        const latestVersion = response[0];
+        const latestVersionArray = latestVersion.split('.');
 
-      const expectedVersionArray = Array.from(latestVersionArray);
-      if (hasModifiedSrcFiles) {
-        expectedVersionArray[1] = String(parseInt(expectedVersionArray[1]) + 1);
-        expectedVersionArray[2] = String(0);
-      } else if (hasAddedSrcFiles) {
-        expectedVersionArray[2] = String(parseInt(expectedVersionArray[2]) + 1);
-      }
-      const expectedVersion = expectedVersionArray.join('.');
+        const expectedVersionArray = Array.from(latestVersionArray);
+        if (hasModifiedSrcFiles) {
+          expectedVersionArray[1] = String(
+            parseInt(expectedVersionArray[1]) + 1
+          );
+          expectedVersionArray[2] = String(0);
+        } else if (hasAddedSrcFiles) {
+          expectedVersionArray[2] = String(
+            parseInt(expectedVersionArray[2]) + 1
+          );
+        }
+        const expectedVersion = expectedVersionArray.join('.');
 
-      if (proposedVersion !== expectedVersion) {
-        console.error(`Proposed version ${proposedVersion} doesn't match expected version ${expectedVersion}!`);
-        process.exit(1);
-      } else {
-        console.info('OK');
-      }
+        if (proposedVersion !== expectedVersion) {
+          console.error(
+            `Proposed version ${proposedVersion} doesn't match expected version ${expectedVersion}!`
+          );
+          process.exit(1);
+        } else {
+          console.info('OK');
+        }
 
-      process.exit(0);
-    });
+        process.exit(0);
+      });
   });
 }
 
